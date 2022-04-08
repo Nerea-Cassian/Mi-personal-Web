5d44ce17fba75b026e83ca3615387fdf4b394466
Nerea Cassian <70113068+NereaCassian@users.noreply.github.com>
2022-04-08 22:39:55 +0200

    Un pequeño easter egg

---
+++ js/videoplay.js
@@ -0,0 +1,11 @@
+let btn =document.getElementById('trustme');
+
+// fuction that shows and plays a video when the button is clicked
+btn.addEventListener('click', function(){
+    let video = document.getElementById('video');
+    btn.style.display = 'none';
+    video.style.display = 'block';
+    video.play();
+});
+
+
